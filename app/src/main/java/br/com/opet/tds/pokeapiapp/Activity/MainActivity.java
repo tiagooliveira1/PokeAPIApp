a3d7c34f94f46a59d20527a2d6a84efbf88180e3
Tiago Oliveira <tiago@econtabil.com>
2018-08-13 22:25:00 -0300

    first commit

---
+++ app/src/main/java/br/com/opet/tds/pokeapiapp/Activity/MainActivity.java
@@ -0,0 +1,112 @@
+package br.com.opet.tds.pokeapiapp.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+import br.com.opet.tds.pokeapiapp.Model.Pokemon;
+import br.com.opet.tds.pokeapiapp.R;
+
+public class MainActivity extends Activity {
+
+    private static final String URL = "https://pokeapi.co/api/v2/pokemon/1";
+
+    private RequestQueue queue;
+    private Gson gson;
+
+    private TextView textID;
+    private TextView textName;
+    private TextView textHeight;
+    private TextView textWeight;
+    private TextView textTypes;
+
+    private EditText edtNameSearch;
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textID = findViewById(R.id.textID);
+        textName = findViewById(R.id.textName);
+        textHeight = findViewById(R.id.textHeight);
+        textWeight = findViewById(R.id.textWeight);
+        textTypes = findViewById(R.id.textTypes);
+        progressBar = findViewById(R.id.progressConnection);
+
+        edtNameSearch = findViewById(R.id.edtNomePokemon);
+
+
+
+        GsonBuilder builder = new GsonBuilder();
+        gson = builder.create();
+        queue = Volley.newRequestQueue(this);
+        callPokemon();
+    }
+
+    private void callPokemon(){
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+        StringRequest request = new StringRequest(Request.Method.GET,URL,onPokemonLoaded,onPokemonError);
+        queue.add(request);
+    }
+
+    private final Response.Listener<String> onPokemonLoaded = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            Pokemon pokemon = gson.fromJson(response,Pokemon.class);
+
+            textID.setText(String.valueOf(pokemon.getID()));
+            textName.setText(pokemon.getName());
+            textHeight.setText(String.valueOf(pokemon.getHeight()));
+            textWeight.setText(String.valueOf(pokemon.getWeight()));
+
+            String stypes = "";
+            for(Pokemon.Types types : pokemon.getTypes()){
+                stypes += types.getType().getName() + " ";
+            }
+
+            textTypes.setText(stypes);
+
+            Log.i("POKERESPONSE",response);
+            progressBar.setVisibility(ProgressBar.GONE);
+            Toast.makeText(MainActivity.this, "Erro ao capturar os dados."+pokemon.get, Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    private final Response.ErrorListener onPokemonError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("POKERESPONSE",error.toString());
+            Toast.makeText(MainActivity.this, "Erro ao capturar os dados."+error.toString(), Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(ProgressBar.GONE);
+            //callPokemon();
+        }
+    };
+
+    public void buscarPokemon(View v)
+    {
+        String urlSearch = "https://pokeapi.co/api/v2/pokemon/"+edtNameSearch.getText().toString();
+        Toast.makeText(MainActivity.this, urlSearch, Toast.LENGTH_SHORT).show();
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+        StringRequest request = new StringRequest(Request.Method.GET,urlSearch,onPokemonLoaded,onPokemonError);
+        queue.add(request);
+    }
+}
---
+++ app/src/main/java/br/com/opet/tds/pokeapiapp/Model/Pokemon.java
@@ -0,0 +1,112 @@
+package br.com.opet.tds.pokeapiapp.Model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Diego on 12/08/2018.
+ */
+
+public class Pokemon {
+
+    @SerializedName("id")
+    private long ID;
+    private String name;
+    private int height;
+    private int weight;
+    private List<Types> types;
+    private 
+
+    public Pokemon() {
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public List<Types> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<Types> types) {
+        this.types = types;
+    }
+
+    public class Types{
+        private Type type;
+
+        public Types() {
+
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type type) {
+            this.type = type;
+        }
+
+        public class Type{
+            private String name;
+
+            public Type() {
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+
+    public Sprites getSprites() { this.sprites; }
+
+    public class Sprites{
+        @SerializedName("front_default")
+        private String frontDefault;
+
+        public Sprites() {
+
+        }
+        public String getFrontDefault() {
+            return frontDefault;
+        }
+        public void setFrontDefault(String frontDefault) {
+            this.frontDefault = frontDefault;
+        }
+    }
+}
